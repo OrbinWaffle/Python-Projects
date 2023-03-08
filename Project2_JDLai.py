92a450582f29c7e0887fae1aef5c61a1872a4ef3
OrbinWaffle <79740022+OrbinWaffle@users.noreply.github.com>
2023-03-08 14:09:46 -0800

    added task loop

--- Project2_JDLai.py
+++ Project2_JDLai.py
@@ -15,8 +15,8 @@ def task_one_main():
     print("Your sentence:", user_input)
     print("This sentence has {0} characters.".format(num_of_chars))
     print("Without whitespace, your sentence is:", no_whitespace)
-    print("Your sentence encrypted is: ", encrypted)
-    print("Your sentence decryped is: ", decrypted)
+    print("Your sentence encrypted is:", encrypted)
+    print("Your sentence decryped is:", decrypted)
     return
     
 
@@ -44,6 +44,35 @@ def get_safe(input_string, key):
 # is identical to the encryption one.
 def go_recover(input_string, key):
     return get_safe(input_string, key)
+
+# TASK ONE OUTPUT:
+# Which task would you like to run? Type 1, 2, or 3: 1
+# Enter a sentence: The only thing we have to fear is fear itself.
+# Your sentence: The only thing we have to fear is fear itself.
+# This sentence has 46 characters.
+# Without whitespace, your sentence is: Theonlythingwehavetofearisfearitself.
+# Your sentence encrypted is: ▼☺◄T
+# ☼♣.▬*↔▬N§♠T♀→.♣↕Z
+# Your sentence decryped is: The only thing we have to fear is fear itself.
+
+# Which task would you like to run? Type 1, 2, or 3: 1
+# Enter an encryption key: William
+# Your sentence: To be, or not to be, that is the question.
+# This sentence has 42 characters.
+# Without whitespace, your sentence is: Tobe,ornottobe,thatisthequestion.
+# ↑▲w↔♦senI►↑2→↑♣♠☼Cypted is: ♥♠L♫♀MM8☻♠§M#♠L♫♀MM#☺
+# Your sentence decryped is: To be, or not to be, that is the question.
+
+# Which task would you like to run? Type 1, 2, or 3: 1
+# Enter a sentence: Python is an interpreted, object-oriented, high-level programming language with dynamic semantics.
+# Enter an encryption key: snake     
+# Your sentence: Python is an interpreted, object-oriented, high-level programming language with dynamic semantics.
+# This sentence has 98 characters.
+# Without whitespace, your sentence is: Pythonisaninterpreted,object-oriented,high-levelprogramminglanguagewithdynamicsemantics.
+# Your sentence encrypted is: #↨§♥
+# K§☺☺♠↓♦▲♣☻S☻♣☻♠☼♠♫E♦§♥E↨↨☼☼▼↨BA♥♀¶♠L♣♂
+# A↑▲☼☼▼♀►↔O
+# Your sentence decryped is: Python is an interpreted, object-oriented, high-level programming language with dynamic semantics.
 
 
 
@@ -84,6 +113,14 @@ def generate_primes(num_of_primes):
             primes_generated += 1
             yield num_to_add
         index += 1
+
+# TASK TWO OUTPUT:
+# Which task would you like to run? Type 1, 2, or 3: 2
+# First 50 prime numbers: 
+# 2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97 101 103 107 109 113 127 131 137 139 149 151 157 163 167 173 179 181 191 193 197 199 211 223 227 229
+# 101st prime number: 547
+# 1001st prime number: 8831
+# It took 0.1860487461090088 seconds to run step two.
 
 
 
@@ -153,11 +190,15 @@ def draw_bar_code(input_code):
 
 
 # Task selector
-selection = int(
-   input("Which task would you like to run? Type 1, 2, or 3: "))
-if (selection == 1):
-   task_one_main()
-elif (selection == 2):
-   task_two_main()
-elif (selection == 3):
-   task_three_main()
+while(True):
+    selection = int(
+        input("Which task would you like to run? Type 1, 2, or 3. Type 0 to quit: "))
+    if (selection == 0):
+        quit()
+    if (selection == 1):
+        task_one_main()
+    elif (selection == 2):
+        task_two_main()
+    elif (selection == 3):
+        task_three_main()
+    print()
